2eb3795ee4184b921ca65100f864866ca646d2ac
Nuintun <nuintun@qq.com>
2015-05-21 16:36:50 +0800

    adjust code style

--- src/console.js
+++ src/console.js
@@ -9,20 +9,20 @@
 this.console = this.console || {};
 
 (function (console, global){
-    var timeStack = {};
+  var timeStack = {};
 
-    global.console.log = console.log || function (){};
+  global.console.log = console.log || function (){};
 
-    global.console.info = console.info || function (){};
+  global.console.info = console.info || function (){};
 
-    global.console.time = console.time || function (name){
-        timeStack[name] = +new Date();
+  global.console.time = console.time || function (name){
+      timeStack[name] = +new Date();
     };
 
-    global.console.timeEnd = console.timeEnd || function (name){
-        if (timeStack[name]) {
-            console.info(name + ': ' + (+new Date() - timeStack[name]) + 'ms');
-            delete timeStack[name];
-        }
+  global.console.timeEnd = console.timeEnd || function (name){
+      if (timeStack[name]) {
+        console.info(name + ': ' + (+new Date() - timeStack[name]) + 'ms');
+        delete timeStack[name];
+      }
     };
 }(console, this));
--- src/observer.js
+++ src/observer.js
@@ -11,261 +11,262 @@ observer._util = {};
 observer._module = {};
 
 (function (util){
-    var AP = Array.prototype;
-    var toString = Object.prototype.toString;
+  var AP = Array.prototype;
+  var toString = Object.prototype.toString;
 
-    util.isString = function (val){
-        return toString.call(val) === '[object String]';
+  util.isString = function (val){
+    return toString.call(val) === '[object String]';
+  };
+
+  util.isFunction = function (val){
+    return toString.call(val) === '[object Function]';
+  };
+
+  util.isArray = Array.isArray || function (val){
+      return toString.call(val) === '[object Array]';
     };
 
-    util.isFunction = function (val){
-        return toString.call(val) === '[object Function]';
-    };
+  var forEach = util.forEach = function (arr, fn){
+    for (var len = arr.length || 0, i = len; i >= 1; --i) {
+      if (fn(arr[len - i], len - i, arr) === false) {
+        break;
+      }
+    }
+  };
 
-    util.isArray = Array.isArray || function (val){
-        return toString.call(val) === '[object Array]';
-    };
+  util.indexOf = AP.indexOf ? function (arr, item){
+    return arr.indexOf(item);
+  } : function (arr, item){
+    var index = -1;
 
-    var forEach = util.forEach = function (arr, fn){
-        for (var len = arr.length || 0, i = len; i >= 1; --i) {
-            if (fn(arr[len - i], len - i, arr) === false) {
-                break;
-            }
-        }
-    };
+    forEach(arr, function (entries, i){
+      if (entries === item) {
+        index = i;
+        return false;
+      }
+    });
 
-    util.indexOf = AP.indexOf ? function (arr, item){
-        return arr.indexOf(item);
-    } : function (arr, item){
-        var index = -1;
+    return index;
+  };
 
-        forEach(arr, function (entries, i){
-            if (entries === item) {
-                index = i;
-                return false;
-            }
-        });
-
-        return index;
-    };
-
-    util.removeItem = function (arr, item){
-        forEach(arr, function (entries, i){
-            if (entries === item) {
-                arr.splice(i, 1);
-                return false;
-            }
-        });
-    };
+  util.removeItem = function (arr, item){
+    forEach(arr, function (entries, i){
+      if (entries === item) {
+        arr.splice(i, 1);
+        return false;
+      }
+    });
+  };
 })(observer._util);
 
 (function (util, module, undefined){
-    var TOPIC_STACK = {
-        length: 0,
-        listeners: []
-    };
-    var TOPIC_RE = /^[A-Z_$][A-Z0-9_$]*(\.[A-Z_$][A-Z0-9_$]*)*$/i;
+  var TOPIC_STACK = {
+    length: 0,
+    listeners: []
+  };
+  var TOPIC_RE = /^[A-Z_$][A-Z0-9_$]*(\.[A-Z_$][A-Z0-9_$]*)*$/i;
 
-    function validTopic(topic){
-        var valid = util.isString(topic) && TOPIC_RE.test(topic);
+  function validTopic(topic){
+    var valid = util.isString(topic) && TOPIC_RE.test(topic);
 
-        if (valid) {
-            return true;
-        } else {
-            throw SyntaxError('Illegal Topic');
-        }
+    if (valid) {
+      return true;
+    } else {
+      throw SyntaxError('Illegal Topic');
     }
+  }
 
-    function setTopic(topic){
-        var tns = TOPIC_STACK;
+  function setTopic(topic){
+    var tns = TOPIC_STACK;
 
-        util.forEach(topic.split('.'), function (topic){
-            if (topic === 'listeners' || topic === 'length') {
-                return;
-            }
+    util.forEach(topic.split('.'), function (topic){
+      if (topic === 'listeners' || topic === 'length') {
+        return;
+      }
 
-            if (!tns[topic]) {
-                tns.length += 1;
-                tns[topic] = {
-                    length: 0,
-                    listeners: []
-                };
-            }
-
-            tns = tns[topic];
-        });
-
-        return tns;
-    }
-
-    function getTopic(topic){
-        var tns = TOPIC_STACK;
-
-        util.forEach(topic.split('.'), function (topic){
-            if (!tns) {
-                return false;
-            }
-
-            if (topic === 'listeners' || topic === 'length') {
-                return;
-            }
-
-            tns = tns[topic] || undefined;
-        });
-
-        return tns;
-    }
-
-    function emptyTopic(tns){
-        return (!tns.length && !tns.listeners.length);
-    }
-
-    function cleanTopic(topic, cns){
-        if (!emptyTopic(cns)) {
-            return;
+      if (!tns[topic]) {
+        tns.length += 1;
+        tns[topic] = {
+          length: 0,
+          listeners: []
         };
+      }
 
-        var lastdot = topic.lastIndexOf('.'),
-            pnsstr = topic.substring(0, lastdot),
-            cnsstr = topic.substring(lastdot + 1),
-            pns = pnsstr ? getTopic(pnsstr) : TOPIC_STACK;
+      tns = tns[topic];
+    });
+
+    return tns;
+  }
+
+  function getTopic(topic){
+    var tns = TOPIC_STACK;
+
+    util.forEach(topic.split('.'), function (topic){
+      if (!tns) {
+        return false;
+      }
+
+      if (topic === 'listeners' || topic === 'length') {
+        return;
+      }
+
+      tns = tns[topic] || undefined;
+    });
+
+    return tns;
+  }
+
+  function emptyTopic(tns){
+    return (!tns.length && !tns.listeners.length);
+  }
+
+  function cleanTopic(topic, cns){
+    if (!emptyTopic(cns)) {
+      return;
+    }
+    ;
+
+    var lastdot = topic.lastIndexOf('.'),
+      pnsstr = topic.substring(0, lastdot),
+      cnsstr = topic.substring(lastdot + 1),
+      pns = pnsstr ? getTopic(pnsstr) : TOPIC_STACK;
+
+    delete pns[cnsstr];
+
+    pns.length -= 1;
+
+    if (pnsstr) {
+      topic = pnsstr;
+
+      for (var i = topic.split('.').length; i >= 1; --i) {
+        lastdot = topic.lastIndexOf('.');
+        pnsstr = topic.substring(0, lastdot);
+        cnsstr = topic.substring(lastdot + 1);
+        cns = getTopic(topic);
+
+        if (!emptyTopic(cns)) {
+          break;
+        }
+
+        pns = pnsstr ? getTopic(pnsstr) : TOPIC_STACK;
 
         delete pns[cnsstr];
 
         pns.length -= 1;
+        topic = pnsstr;
+      }
+    }
+  }
 
-        if (pnsstr) {
-            topic = pnsstr;
+  function walk(tns, message, topic, publisher){
+    util.forEach(tns.listeners, function (fn){
+      fn(message, topic, publisher);
+    });
 
-            for (var i = topic.split('.').length; i >= 1; --i) {
-                lastdot = topic.lastIndexOf('.');
-                pnsstr = topic.substring(0, lastdot);
-                cnsstr = topic.substring(lastdot + 1);
-                cns = getTopic(topic);
+    for (var i in tns) {
+      if (!tns.hasOwnProperty(i) || i === 'listeners' || i === 'length') {
+        continue;
+      }
 
-                if (!emptyTopic(cns)) {
-                    break;
-                }
+      walk(tns[i], message, topic + '.' + i, publisher);
+    }
+  }
 
-                pns = pnsstr ? getTopic(pnsstr) : TOPIC_STACK;
+  function notify(message, topic){
+    var tns = getTopic(topic);
 
-                delete pns[cnsstr];
-
-                pns.length -= 1;
-                topic = pnsstr;
-            }
-        }
+    if (!tns) {
+      return;
     }
 
-    function walk(tns, message, topic, publisher){
-        util.forEach(tns.listeners, function (fn){
-            fn(message, topic, publisher);
-        });
+    walk(tns, message, topic, topic);
+  }
 
-        for (var i in tns) {
-            if (!tns.hasOwnProperty(i) || i === 'listeners' || i === 'length') {
-                continue;
-            }
+  module.subscribe = function (topic, fn){
+    topic = util.isArray(topic) ? topic : [topic];
 
-            walk(tns[i], message, topic + '.' + i, publisher);
-        }
-    }
+    util.forEach(topic, function (topic){
+      if (!validTopic(topic)) {
+        return;
+      }
 
-    function notify(message, topic){
-        var tns = getTopic(topic);
+      var tns = setTopic(topic);
+      var listeners = tns.listeners;
 
-        if (!tns) {
-            return;
-        }
+      fn = util.isArray(fn) ? fn : [fn];
 
-        walk(tns, message, topic, topic);
-    }
+      util.forEach(fn, function (fn){
+        util.isFunction(fn) && util.indexOf(listeners, fn) === -1 && listeners.push(fn);
+      });
+    });
+  };
 
-    module.subscribe = function (topic, fn){
-        topic = util.isArray(topic) ? topic : [topic];
+  module.publish = function (topic, message){
+    topic = util.isArray(topic) ? topic : [topic];
 
-        util.forEach(topic, function (topic){
+    util.forEach(topic, function (topic){
+      validTopic(topic) && notify(message, topic);
+    });
+  };
+
+  module.unsubscribe = function (topic, fn){
+    if (!arguments.length) {
+      TOPIC_STACK = {
+        length: 0,
+        listeners: []
+      };
+    } else {
+      topic = util.isArray(topic) ? topic : [topic];
+
+      switch (arguments.length) {
+        case 1:
+          util.forEach(topic, function (topic){
             if (!validTopic(topic)) {
-                return;
+              return;
             }
 
-            var tns = setTopic(topic);
-            var listeners = tns.listeners;
+            var tns = getTopic(topic);
 
+            if (!tns) {
+              return;
+            }
+
+            tns.listeners = [];
+
+            cleanTopic(topic, tns);
+          });
+          break;
+        default:
+          util.forEach(topic, function (topic){
+            if (!validTopic(topic)) {
+              return;
+            }
+
+            var tns = getTopic(topic);
+
+            if (!tns) {
+              return;
+            }
+
+            var listeners = tns.listeners;
             fn = util.isArray(fn) ? fn : [fn];
 
             util.forEach(fn, function (fn){
-                util.isFunction(fn) && util.indexOf(listeners, fn) === -1 && listeners.push(fn);
+              util.removeItem(listeners, fn);
             });
-        });
-    };
 
-    module.publish = function (topic, message){
-        topic = util.isArray(topic) ? topic : [topic];
-
-        util.forEach(topic, function (topic){
-            validTopic(topic) && notify(message, topic);
-        });
-    };
-
-    module.unsubscribe = function (topic, fn){
-        if (!arguments.length) {
-            TOPIC_STACK = {
-                length: 0,
-                listeners: []
-            };
-        } else {
-            topic = util.isArray(topic) ? topic : [topic];
-
-            switch (arguments.length) {
-                case 1:
-                    util.forEach(topic, function (topic){
-                        if (!validTopic(topic)) {
-                            return;
-                        }
-
-                        var tns = getTopic(topic);
-
-                        if (!tns) {
-                            return;
-                        }
-
-                        tns.listeners = [];
-
-                        cleanTopic(topic, tns);
-                    });
-                    break;
-                default:
-                    util.forEach(topic, function (topic){
-                        if (!validTopic(topic)) {
-                            return;
-                        }
-
-                        var tns = getTopic(topic);
-
-                        if (!tns) {
-                            return;
-                        }
-
-                        var listeners = tns.listeners;
-                        fn = util.isArray(fn) ? fn : [fn];
-
-                        util.forEach(fn, function (fn){
-                            util.removeItem(listeners, fn);
-                        });
-
-                        cleanTopic(topic, tns);
-                    });
-                    break;
-            }
-        }
-    };
+            cleanTopic(topic, tns);
+          });
+          break;
+      }
+    }
+  };
 }(observer._util, observer._module));
 
 (function (observer, global){
-    global.observer.subscribe = observer._module.subscribe;
-    global.observer.publish = observer._module.publish;
-    global.observer.unsubscribe = observer._module.unsubscribe;
-    delete observer._util;
-    delete observer._module;
+  global.observer.subscribe = observer._module.subscribe;
+  global.observer.publish = observer._module.publish;
+  global.observer.unsubscribe = observer._module.unsubscribe;
+  delete observer._util;
+  delete observer._module;
 }(observer, this));
